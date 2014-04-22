77ee39cac55d7d2f7081a66d67cc4efffa0bb399
mtambo <mtambo@uslt90836.com>
2014-04-21 20:37:48 -0500

    add ssl test

---
+++ SSLTest.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+
+import javax.net.ssl.SSLSocketFactory;
+
+  public class SSLTest {
+        
+     public static final String TARGET_HTTPS_SERVER = "esbqa-med.intra.searshc.com"; 
+     public static final int    TARGET_HTTPS_PORT   = 443; 
+        
+     public static void main(String[] args) throws Exception {
+    	 
+/*    	 System.setProperty("javax.net.ssl.trustStore", "/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.60-2.4.7.0.fc20.x86_64/jre/lib/security/cacerts");
+    	 System.setProperty("javax.net.ssl.trustStorePassword", "changeit");*/
+    	 
+    	 System.setProperty("javax.net.debug", "ssl");
+       Socket socket = SSLSocketFactory.getDefault().
+         createSocket(TARGET_HTTPS_SERVER, TARGET_HTTPS_PORT);
+       try {
+         Writer out = new OutputStreamWriter(
+            socket.getOutputStream(), "ISO-8859-1");
+         out.write("GET / HTTP/1.1\r\n");  
+         out.write("Host: " + TARGET_HTTPS_SERVER + ":" + 
+             TARGET_HTTPS_PORT + "\r\n");  
+         out.write("Agent: SSL-TEST\r\n");  
+         out.write("\r\n");  
+         out.flush();  
+         BufferedReader in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream(), "ISO-8859-1"));
+         String line = null;
+         while ((line = in.readLine()) != null) {
+            System.out.println(line);
+         }
+       } finally {
+         socket.close(); 
+       }
+     }
+  }
